512c60e50e623f70c527376cde786d3785e99d74
christinakarl <christina.karl@posteo.de>
2024-03-14 13:54:03 +0100

    mod08/ex00

--- Module08/ex00/Easyfind.hpp
+++ Module08/ex00/Easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: ckarl <ckarl@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 18:10:04 by ckarl             #+#    #+#             */
-/*   Updated: 2024/03/13 18:21:04 by ckarl            ###   ########.fr       */
+/*   Updated: 2024/03/14 12:10:47 by ckarl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,24 @@
 #include <iostream>
 #include <string>
 
+class	NotFoundError : public std::exception
+{
+public:
+	virtual const char *what(void) const throw()
+	{
+		return "Instance could not be found in the container";
+	};
+};
 
 template <typename T>
+void	easyfind(T &cont, int i)
+{
+	typename T::iterator	index = std::find(cont.begin(), cont.end(), i);
+	if (index != cont.end())
+		std::cout << "Instance was found" << std::endl;
+	else
+		throw NotFoundError();
+}
 
 
 #endif
--- Module08/ex00/main.cpp
+++ Module08/ex00/main.cpp
@@ -6,15 +6,54 @@
 /*   By: ckarl <ckarl@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 15:34:04 by ckarl             #+#    #+#             */
-/*   Updated: 2024/03/13 18:10:27 by ckarl            ###   ########.fr       */
+/*   Updated: 2024/03/14 12:19:29 by ckarl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Easyfind.hpp"
 #include <cstdlib>
+#include <vector>
+#include <deque>
+#include <list>
 
 int	main()
 {
-	
+	{
+		std::vector<int>	test;
+		std::vector<int>	test1;
+
+		test.push_back(2); test.push_back(5); test.push_back(8); test.push_back(9);
+		try {
+			easyfind(test, 5);
+		}
+		catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+	}
+	{
+		std::deque<unsigned int> test3;
+		std::deque<unsigned int> test4;
+
+		test3.push_back(44); test3.push_back(3); test3.push_back(0);
+		try {
+			easyfind(test4, 3);
+		}
+		catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+	}
+	{
+		std::list<char> test5;
+		std::list<char> test6;
+
+		test5.push_back(44); test5.push_back(3); test5.push_back(0);
+		try {
+			easyfind(test6, 3);
+		}
+		catch (std::exception &e) {
+			std::cout << e.what() << std::endl;
+		}
+	}
+
 	return 0;
 }
