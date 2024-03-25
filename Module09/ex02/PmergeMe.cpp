816c8fdb39c0aa2d0bfd5820235fa12b74e1e57a
christinakarl <christina.karl@posteo.de>
2024-03-25 11:17:08 +0100

    mod09/ex02, just time is too big

--- Module09/ex02/PmergeMe.cpp
+++ Module09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: ckarl <ckarl@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 16:35:31 by ckarl             #+#    #+#             */
-/*   Updated: 2024/03/22 18:41:33 by ckarl            ###   ########.fr       */
+/*   Updated: 2024/03/25 11:16:49 by ckarl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,10 @@ PmergeMe &PmergeMe::operator = (const PmergeMe &c)
 	if (this != &c)
 	{
 		_deq = c._deq;
-		_lst = c._lst;
+		_vec = c._vec;
 		_deq_time = c._deq_time;
-		_lst_time = c._lst_time;
+		_vec_time = c._vec_time;
+		_unsorted = c._unsorted;
 	}
 	return *this;
 }
@@ -35,9 +36,6 @@ bool	PmergeMe::valid_input(const std::string &arg)
 	std::istringstream	ss(arg);
 	char c;
 
-	ss >> c;
-	if (c == '-')
-		return false;
 	while (ss >> c)
 	{
 		if (c < '0' || c > '9')
@@ -57,29 +55,46 @@ void	PmergeMe::add(const std::string &arg)
 	if (n > INT_MAX)
 		throw invalidInput();
 	_deq.push_back(n);
-	_lst.push_back(n);
+	_vec.push_back(n);
 }
 
 
 void	PmergeMe::sort_all()
 {
-	// struct timeval begin, end;
+	struct timeval begin, end;
 
-	// gettimeofday(&begin, 0);
+	_unsorted = _vec;
+	gettimeofday(&begin, NULL);
+	// std::chrono::time_point<std::chrono::high_resolution_clock> start_time = std::chrono::high_resolution_clock::now();
+	if (_deq.empty())
+		throw emptyContainer();
 	_deq = merge_sort_deq(_deq);
-	// gettimeofday(&end, 0);
-	// _deq_time = (end.tv_sec - begin.tv_sec) + (end.tv_usec - begin.tv_sec) * 1e-6;
+	// std::chrono::time_point<std::chrono::high_resolution_clock> end_time = std::chrono::high_resolution_clock::now();
+	gettimeofday(&end, NULL);
+	_deq_time = (end.tv_sec - begin.tv_sec) * 1000 + (end.tv_usec - begin.tv_usec);
+	// std::chrono::duration<double> elapsed_seconds_deq = end_time - start_time;
 	// std::cout << "_deq_time: " << _deq_time << " us" << std::endl;
+
+	gettimeofday(&begin, NULL);
+	// std::chrono::time_point<std::chrono::high_resolution_clock> start_time = std::chrono::high_resolution_clock::now();
+	if (_vec.empty())
+		throw emptyContainer();
+	_vec = merge_sort_vec(_vec);
+	// std::chrono::time_point<std::chrono::high_resolution_clock> end_time = std::chrono::high_resolution_clock::now();
+	gettimeofday(&end, NULL);
+	_vec_time = (end.tv_sec - begin.tv_sec) * 1000 + (end.tv_usec - begin.tv_usec);
+	// std::chrono::duration<double> elapsed_seconds_deq = end_time - start_time;
+	// std::cout << "_deq_time: " << _vec_time << " us" << std::endl;
+	displayAll();
 }
 
-std::deque<int>	PmergeMe::merge_sort_deq(std::deque<int> d)
+std::deque<int>	PmergeMe::merge_sort_deq(std::deque<int> &d)
 {
 	if (d.size() <= 1)
 		return d;
 	size_t middle = d.size() / 2;
-	std::deque<int> left(d.begin(), _deq.begin() + middle);
-	
-	std::deque<int> right(d.begin() + middle, d.end());
+	std::deque<int> left(d.begin(), (d.begin() + middle));
+	std::deque<int> right((d.begin() + middle), d.end());
 
 	left = merge_sort_deq(left);
 	right = merge_sort_deq(right);
@@ -89,16 +104,55 @@ std::deque<int>	PmergeMe::merge_sort_deq(std::deque<int> d)
 	return result;
 }
 
-void	PmergeMe::sort_lst()
+std::vector<int>	PmergeMe::merge_sort_vec(std::vector<int> &l)
 {
+	if (l.size() <= 1)
+		return l;
+	size_t middle = l.size() / 2;
+	std::vector<int> left(l.begin(), (l.begin() + middle));
+	std::vector<int> right((l.begin() + middle), l.end());
 
+	left = merge_sort_vec(left);
+	right = merge_sort_vec(right);
+
+	std::vector<int> result;
+	std::merge(left.begin(), left.end(), right.begin(), right.end(), std::back_inserter(result));
+	return result;
 }
 
+void	PmergeMe::displayAll()
+{
+	int i = 0;
+
+	std::cout << "Before: ";
+	for (std::vector<int>::iterator it = _unsorted.begin(); it != _unsorted.end(); it++, i++) {
+		std::cout << *it << " ";
+		if (i == 3) {
+			std::cout << "[...]"; break;
+		}
+	}
+	std::cout << std::endl;
+	i = 0;
+	std::cout << "After:  ";
+	for (std::vector<int>::iterator it = _vec.begin(); it != _vec.end(); it++, i++) {
+		std::cout << *it << " ";
+		if (i == 3) {
+			std::cout << "[...]"; break;
+		}
+	}
+	std::cout << std::endl;
+	std::cout << "Time to process a range of " << _vec.size() << " elements with std::vector<int>: " << _vec_time << " us" << std::endl;
+	std::cout << "Time to process a range of " << _deq.size() << " elements with std::deque<int>: " << _deq_time << " us" << std::endl;
+}
+
+
 const char	*PmergeMe::invalidInput::what(void) const throw() { return "The argument list contains invalid input"; }
+const char	*PmergeMe::emptyContainer::what(void) const throw() { return "Cannot merge sort an empty container"; }
+
 
 std::ostream	&operator << (std::ostream &os, const PmergeMe &obj)
 {
-	//display PmergeMe deq, lst and time
 	(void)obj;
+
 	return os;
 }
--- Module09/ex02/PmergeMe.hpp
+++ Module09/ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: ckarl <ckarl@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 16:35:27 by ckarl             #+#    #+#             */
-/*   Updated: 2024/03/22 18:36:48 by ckarl            ###   ########.fr       */
+/*   Updated: 2024/03/25 11:03:20 by ckarl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 #include <exception>
 #include <list>
 #include <deque>
+#include <vector>
 #include <sstream>
 #include <sys/time.h>
 
@@ -33,8 +34,8 @@ public:
 	void	add(const std::string &arg);
 	bool	valid_input(const std::string &arg);
 	void	sort_all();
-	std::deque<int>		merge_sort_deq(std::deque<int> _deq);
-	void	sort_lst();
+	std::deque<int>		merge_sort_deq(std::deque<int> &d);
+	std::vector<int>	merge_sort_vec(std::vector<int> & l);
 	//sort deque
 	//sort list
 	class invalidInput : public std::exception
@@ -42,14 +43,26 @@ public:
 		public:
 			virtual const char	*what(void) const throw();
 	};
+	class emptyContainer : public std::exception
+	{
+		public:
+			virtual const char	*what(void) const throw();
+	};
+
+	void	displayAll();
+
+	// std::deque<int>		getDeq();
+	// std::vector<int>	getVec();
+	// std::vector<int>	getUnsorted();
+	// long				getDeqTime();
+	// long				getVecTime();
 
 private:
-	std::deque<int>	_deq;
-	std::deque<int>	_deq_sorted;
-	double			_deq_time;
-	std::list<int>	_lst;
-	std::list<int>	_lst_sorted;
-	double			_lst_time;
+	std::deque<int>		_deq;
+	long				_deq_time;
+	std::vector<int>	_vec;
+	long				_vec_time;
+	std::vector<int>	_unsorted;
 
 
 };
