24bd5378cded5922808462b12b7e9e4f8d928f6d
Christina Karl <ckarl@c1r5s3.42lausanne.ch>
2024-03-27 11:41:34 +0100

    cpp06_ready

--- Module06/ex02/main.cpp
+++ Module06/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ckarl <ckarl@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 15:34:04 by ckarl             #+#    #+#             */
-/*   Updated: 2024/02/05 19:01:44 by ckarl            ###   ########.fr       */
+/*   Updated: 2024/03/27 11:41:03 by ckarl            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,17 @@
 //randomly instanciates A, B or C and returns the instance as a Base pointer
 Base	*generate(void)
 {
-	srand (time(NULL));
 	int random = rand() % 3;
 	switch (random)
 	{
 		case 0:
-			std::cout << "A class" << std::endl;
+			// std::cout << "A class" << std::endl;
 			return new A();
 		case 1:
-			std::cout << "B class" << std::endl;
+			// std::cout << "B class" << std::endl;
 			return new B();
 		case 2:
-			std::cout << "C class" << std::endl;
+			// std::cout << "C class" << std::endl;
 			return new C();
 		default:
 			return new Base();
@@ -56,10 +55,12 @@ void identify(Base& p)
 {
 	try {
 		A &testA = dynamic_cast<A&>(p);
+		(void)testA;
 	}
 	catch (std::bad_cast &b){
 		try {
 		B &testB = dynamic_cast<B&>(p);
+		(void)testB;
 		}
 		catch (std::bad_cast &b) {
 			std::cout << "it's C" << std::endl;
@@ -73,13 +74,26 @@ void identify(Base& p)
 
 int	main(void)
 {
+	srand (time(NULL));
 	Base *someClass = generate();
+	Base *test2 = generate();
+	Base *test3 = generate();
+	Base *test4 = generate();
+	Base *test5 = generate();
 	Base &otherClass = *someClass;
-	
+
 	identify(someClass);
-	// identify(otherClass);
+	identify(test2);
+	identify(test3);
+	identify(test4);
+	identify(test5);
+	identify(otherClass);
 
 	delete someClass;
+	delete test2;
+	delete test3;
+	delete test4;
+	delete test5;
 
 	return 0;
 }
